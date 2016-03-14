857582d50b3c3d483c6c49451b3c9dccda111d94
mailmindlin <liamttlyrox@gmail.com>
2016-03-14 15:01:16 -0400

    More RTC

--- js/peer.js
+++ js/peer.js
@@ -6,45 +6,19 @@ window.RTCSessionDescription = window.RTCSessionDescription || window.mozRTCSess
 	//var firebaseRoot = new Firebase("https://shining-torch-5897.firebaseio.com/");
 	//var games = firebaseRoot.child("games");
 	window.JeopardyGame = {
-		peerConnectionConfig: {
-			'iceServers': [
-				{'url': 'stun:stun.services.mozilla.com'},
-				{'url': 'stun:stun.l.google.com:19302'}
-			]
-		},
-		_gotIceCandidate: function(event) {
-			if(event.candidate != null) {
-				console.log(JSON.stringify({'ice': event.candidate}));
-			}
-		},
-		_gotRemoteStream: function(stream) {
-			console.log('stream: ', stream);
-		},
-		_onSDP: function(sdp) {
-			console.log('sdp',sdp);
-		},
-		_gotDescription: function(description) {
-			console.log('description', description);
-			this.peerConnection.setLocalDescription(description, function () {
-				console.log(JSON.stringify({'sdp': description}));
-			}, function() {console.log('set description error')});
-		},
-		_createOfferError: function(descr) {
-			console.error('offer error', descr);
-		},
 		createGame: function(name) {
-			
+			this.connection = new DataConnection();
+			this.connection.onopen = this._onConnect.bind(this);
+			this.connection.onmessage = this._onMessage.bind(this);
+		},
+		_onConnect: function(e) {
+			console.log(e);
+		},
+		_onMessage: function(msg, uid) {
+			console.log(arguments);
 		},
 		joinGame: function(game) {
 			
-		},
-		_start: function(isCaller) {
-			this.peerConnection = new RTCPeerConnection(this.peerConnectionConfig);
-			this.peerConnection.onicecandidate = this._gotIceCandidate.bind(this);
-			this.peerConnection.onaddstream = this._gotRemoteStream.bind(this);
-			if(isCaller) {
-				this.peerConnection.createOffer(this._gotDescription.bind(this), this._createOfferError.bind(this));
-			}
 		},
 		writeObject: function(object) {
 			
