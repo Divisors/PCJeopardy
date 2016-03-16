1caf0882c2734a4b945558bb9ba9374c7be68966
mailmindlin <liamttlyrox@gmail.com>
2016-03-16 12:35:44 -0400

    Updated RTC

--- js/peer.js
+++ js/peer.js
@@ -5,9 +5,15 @@ window.RTCSessionDescription = window.RTCSessionDescription || window.mozRTCSess
 (function() {
 	//var firebaseRoot = new Firebase("https://shining-torch-5897.firebaseio.com/");
 	//var games = firebaseRoot.child("games");
+	var peerConfig = {"iceServers": [{"url": "stun:stun.l.google.com:19302"}]};
 	window.JeopardyGame = {
 		createGame: function(name) {
-			this.connection = new DataConnection();
+			var connection = this.connection = new RTCPeerConnection(peerConfig, {optional: [{RtpDataChannels: true}]});
+			try {
+				channel = connection.createDataChannel("jeopardyEvtChannel", {reliable: false});
+			} catch (e) {
+				console.error(e);
+			}
 			this.connection.onopen = this._onConnect.bind(this);
 			this.connection.onmessage = this._onMessage.bind(this);
 		},
